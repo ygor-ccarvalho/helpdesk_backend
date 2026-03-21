1ac958c1ee97c64e41a826422bdc0fa306715dc8
Ygor César de Carvalho <109998784+ygor-ccarvalho@users.noreply.github.com>
2026-03-21 18:09:48 -0300

    Feat: Cria o endpoint findById para tecnico.

---
+++ src/main/java/com/ygor/helpdesk/resources/TecnicoResource.java
@@ -0,0 +1,27 @@
+package com.ygor.helpdesk.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ygor.helpdesk.domain.Tecnico;
+import com.ygor.helpdesk.services.TecnicoService;
+
+@RestController
+@RequestMapping(value = "/tecnicos")
+public class TecnicoResource {
+	
+	@Autowired
+	private TecnicoService service;
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Tecnico> findById(@PathVariable Integer id){
+
+		Tecnico obj = this.service.findById(id);
+		return ResponseEntity.ok().body(obj);
+		
+	}
+}
