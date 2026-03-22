9c7f6942f88225ff750ba344aac0d072726e8a88
Ygor César de Carvalho <109998784+ygor-ccarvalho@users.noreply.github.com>
2026-03-22 13:50:21 -0300

    Feat: cria o endpoint create para Tecnico.

--- src/main/java/com/ygor/helpdesk/domain/Tecnico.java
+++ src/main/java/com/ygor/helpdesk/domain/Tecnico.java
@@ -2,11 +2,13 @@ package com.ygor.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.ygor.helpdesk.domain.dtos.TecnicoDTO;
 import com.ygor.helpdesk.domain.enums.Perfil;
 
 @Entity
@@ -29,6 +31,19 @@ public class Tecnico extends Pessoa{
 		addPerfil(Perfil.CLIENTE);
 	}
 
+	public Tecnico(TecnicoDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		
+		this.dataCriacao = obj.getDataCriacao();
+	}
+	
+	
 	public List<Chamado> getChamados() {
 		return chamados;
 	}
--- src/main/java/com/ygor/helpdesk/services/TecnicoService.java
+++ src/main/java/com/ygor/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ygor.helpdesk.domain.Tecnico;
+import com.ygor.helpdesk.domain.dtos.TecnicoDTO;
 import com.ygor.helpdesk.repositories.TecnicoRepository;
 import com.ygor.helpdesk.services.exceptions.ObjectnotFoundException;
 
@@ -24,5 +25,12 @@ public class TecnicoService {
 
 	public List<Tecnico> findAll() {
 		return repository.findAll();
+	}
+
+	public Tecnico create(TecnicoDTO objDTO) {
+
+		objDTO.setId(null);
+		Tecnico newObj = new Tecnico(objDTO);
+		return repository.save(newObj);
 	}
 }
