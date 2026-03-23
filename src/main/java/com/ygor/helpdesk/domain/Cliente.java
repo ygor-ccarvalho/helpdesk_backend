7bff9276cbf4387d38a39849328329cbbf46b12e
Ygor César de Carvalho <109998784+ygor-ccarvalho@users.noreply.github.com>
2026-03-23 13:11:03 -0300

    Feat: Cria o CRUD para cliente.

--- src/main/java/com/ygor/helpdesk/domain/Cliente.java
+++ src/main/java/com/ygor/helpdesk/domain/Cliente.java
@@ -2,11 +2,13 @@ package com.ygor.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.ygor.helpdesk.domain.dtos.ClienteDTO;
 import com.ygor.helpdesk.domain.enums.Perfil;
 
 @Entity
@@ -23,6 +25,18 @@ public class Cliente extends Pessoa{
 	public Cliente() {
 		super();
 		addPerfil(Perfil.CLIENTE);
+	}
+	
+	public Cliente(ClienteDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		
+		this.dataCriacao = obj.getDataCriacao();
 	}
 
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
---
+++ src/main/java/com/ygor/helpdesk/services/ClienteService.java
@@ -0,0 +1,74 @@
+package com.ygor.helpdesk.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ygor.helpdesk.domain.Pessoa;
+import com.ygor.helpdesk.domain.Cliente;
+import com.ygor.helpdesk.domain.dtos.ClienteDTO;
+import com.ygor.helpdesk.repositories.PessoaRepository;
+import com.ygor.helpdesk.repositories.ClienteRepository;
+import com.ygor.helpdesk.services.exceptions.DataIntegrityViolationException;
+import com.ygor.helpdesk.services.exceptions.ObjectnotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repository;
+	@Autowired
+	private PessoaRepository pessoaRepositoriy;
+
+	public Cliente findById(Integer id) {
+		Optional<Cliente> obj = repository.findById(id);
+
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! Id: " + id));
+	}
+
+	public List<Cliente> findAll() {
+		return repository.findAll();
+	}
+
+	public Cliente create(ClienteDTO objDTO) {
+		objDTO.setId(null);
+		validaPorCpfEEmail(objDTO);
+		Cliente newObj = new Cliente(objDTO);
+		return repository.save(newObj);
+	}
+
+	public Cliente update(Integer id, @Valid ClienteDTO objDTO) {
+		objDTO.setId(id);
+		Cliente oldObj = findById(id);
+		validaPorCpfEEmail(objDTO);
+		oldObj = new Cliente(objDTO);
+		return repository.save(oldObj);
+
+	}
+
+	public void delete(Integer id) {
+		Cliente obj = findById(id);
+		if (obj.getChamados().size() > 0) {
+			throw new DataIntegrityViolationException("O Técnico possui ordens de serviço e não pode ser deletado!");
+		}
+		 
+		repository.deleteById(id);
+	}
+
+	private void validaPorCpfEEmail(ClienteDTO objDTO) {
+		Optional<Pessoa> obj = pessoaRepositoriy.findByCpf(objDTO.getCpf());
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("CPF já cadastrado no sistema!");
+		}
+
+		obj = pessoaRepositoriy.findByEmail(objDTO.getEmail());
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("E-mail já cadastrado no sistema!");
+		}
+	}
+
+}
