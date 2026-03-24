e74bddb3688c99160e734c122951a3c92122c24a
Ygor César de Carvalho <109998784+ygor-ccarvalho@users.noreply.github.com>
2026-03-23 22:09:27 -0300

    Feat: Cria o findById para o chamado.

---
+++ src/main/java/com/ygor/helpdesk/services/ChamadoService.java
@@ -0,0 +1,25 @@
+package com.ygor.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ygor.helpdesk.domain.Chamado;
+import com.ygor.helpdesk.repositories.ChamadoRepository;
+import com.ygor.helpdesk.services.exceptions.ObjectnotFoundException;
+
+@Service
+public class ChamadoService {
+
+	@Autowired
+	private ChamadoRepository repository;
+	
+
+	public Chamado findById(Integer id) {
+		Optional<Chamado> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! ID: " + id));
+	}
+
+	
+}
