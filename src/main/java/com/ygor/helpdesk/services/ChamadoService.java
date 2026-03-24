c6704d2b0f8859cabeb310bd8aa344a443582351
Ygor César de Carvalho <109998784+ygor-ccarvalho@users.noreply.github.com>
2026-03-24 12:49:56 -0300

    Feat: Cria FindAll para chamado.

--- src/main/java/com/ygor/helpdesk/services/ChamadoService.java
+++ src/main/java/com/ygor/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.ygor.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,10 @@ public class ChamadoService {
 	public Chamado findById(Integer id) {
 		Optional<Chamado> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! ID: " + id));
+	}
+
+	public List<Chamado> findAll() {
+		return repository.findAll();
 	}
 
 	
