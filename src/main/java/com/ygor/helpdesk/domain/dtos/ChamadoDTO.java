b035a4c684bf11495addc7be4a001819255c42dd
Ygor César de Carvalho <109998784+ygor-ccarvalho@users.noreply.github.com>
2026-07-01 12:11:18 -0300

    refactor: usa anotações Lombok no ChamadoDTO

--- src/main/java/com/ygor/helpdesk/domain/dtos/ChamadoDTO.java
+++ src/main/java/com/ygor/helpdesk/domain/dtos/ChamadoDTO.java
@@ -3,165 +3,62 @@ package com.ygor.helpdesk.domain.dtos;
 import java.io.Serializable;
 import java.time.LocalDate;
 
-import jakarta.validation.constraints.NotNull;
-
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.ygor.helpdesk.domain.Chamado;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class ChamadoDTO implements Serializable {
-	private static final long serialVersionUID = 1L;
 
-	private Integer id;
-	
-	@JsonFormat(pattern = "dd/MM/yyyy")
-	private  LocalDate dataAbertura = LocalDate.now();
-	@JsonFormat(pattern = "dd/MM/yyyy")
-	private LocalDate dataFechamento;
-	@NotNull(message = "O campo Prioridade é obrigatório")
-	private Integer prioridade;
-	@NotNull(message = "O campo Status é obrigatório")
-	private Integer status;
-	@NotNull(message = "O campo Título é obrigatório")
-	private String titulo;
-	@NotNull(message = "O campo Observações é obrigatório")
-	private String observacoes;
-	@NotNull(message = "O campo Tencico é obrigatório")
-	private Integer tecnico;
-	@NotNull(message = "O campo Cliente é obrigatório")
-	private Integer cliente;
-	
-	private String nomeTecnico;
-	
-	private String nomeCliente;
-	public ChamadoDTO() {
-		super();
-	}
-	
-	
-	public ChamadoDTO(Chamado obj) {
-		this.id = obj.getId();
-		this.dataAbertura = obj.getDataAbertura();
-		this.dataFechamento = obj.getDataFechamento();
-		this.prioridade = obj.getPrioridade().getCodigo();
-		this.status = obj.getStatus().getCodigo();
-		this.titulo = obj.getTitulo();
-		this.observacoes = obj.getObservacoes();
-		this.tecnico = obj.getTecnico().getId();
-		this.cliente = obj.getCliente().getId();
-		this.nomeCliente = obj.getCliente().getNome();
-		this.nomeTecnico = obj.getTecnico().getNome();
-	}
+    private static final long serialVersionUID = 1L;
 
+    private Integer id;
 
-	public Integer getId() {
-		return id;
-	}
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dataAbertura = LocalDate.now();
 
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dataFechamento;
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    @NotNull(message = "O campo Prioridade é obrigatório")
+    private Integer prioridade;
 
+    @NotNull(message = "O campo Status é obrigatório")
+    private Integer status;
 
-	public LocalDate getDataAbertura() {
-		return dataAbertura;
-	}
+    @NotBlank(message = "O campo Título é obrigatório")
+    private String titulo;
 
+    @NotBlank(message = "O campo Observações é obrigatório")
+    private String observacoes;
 
-	public void setDataAbertura(LocalDate dataAbertura) {
-		this.dataAbertura = dataAbertura;
-	}
+    @NotNull(message = "O campo Técnico é obrigatório")
+    private Integer tecnico;
 
+    @NotNull(message = "O campo Cliente é obrigatório")
+    private Integer cliente;
 
-	public LocalDate getDataFechamento() {
-		return dataFechamento;
-	}
+    private String nomeTecnico;
+    private String nomeCliente;
 
-
-	public void setDataFechamento(LocalDate dataFechamento) {
-		this.dataFechamento = dataFechamento;
-	}
-
-
-	public Integer getPrioridade() {
-		return prioridade;
-	}
-
-
-	public void setPrioridade(Integer prioridade) {
-		this.prioridade = prioridade;
-	}
-
-
-	public Integer getStatus() {
-		return status;
-	}
-
-
-	public void setStatus(Integer status) {
-		this.status = status;
-	}
-
-
-	public String getTitulo() {
-		return titulo;
-	}
-
-
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-
-
-	public String getObservacoes() {
-		return observacoes;
-	}
-
-
-	public void setObservacoes(String observacoes) {
-		this.observacoes = observacoes;
-	}
-
-
-	public Integer getTecnico() {
-		return tecnico;
-	}
-
-
-	public void setTecnico(Integer tecnico) {
-		this.tecnico = tecnico;
-	}
-
-
-	public Integer getCliente() {
-		return cliente;
-	}
-
-
-	public void setCliente(Integer cliente) {
-		this.cliente = cliente;
-	}
-
-
-	public String getNomeTecnico() {
-		return nomeTecnico;
-	}
-
-
-	public void setNomeTecnico(String nomeTecnico) {
-		this.nomeTecnico = nomeTecnico;
-	}
-
-
-	public String getNomeCliente() {
-		return nomeCliente;
-	}
-
-
-	public void setNomeCliente(String nomeCliente) {
-		this.nomeCliente = nomeCliente;
-	}
-	
-	
-	
+    public ChamadoDTO(Chamado obj) {
+        this.id = obj.getId();
+        this.dataAbertura = obj.getDataAbertura();
+        this.dataFechamento = obj.getDataFechamento();
+        this.prioridade = obj.getPrioridade().getCodigo();
+        this.status = obj.getStatus().getCodigo();
+        this.titulo = obj.getTitulo();
+        this.observacoes = obj.getObservacoes();
+        this.tecnico = obj.getTecnico().getId();
+        this.cliente = obj.getCliente().getId();
+        this.nomeCliente = obj.getCliente().getNome();
+        this.nomeTecnico = obj.getTecnico().getNome();
+    }
 }
