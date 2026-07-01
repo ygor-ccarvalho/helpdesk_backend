cbe4f840c3f57bc3eacf7af3f323fc0d9b43f2cb
Ygor César de Carvalho <109998784+ygor-ccarvalho@users.noreply.github.com>
2026-07-01 17:04:57 -0300

    refactor: aplica lombok na entidade Pessoa

--- src/main/java/com/ygor/helpdesk/domain/Pessoa.java
+++ src/main/java/com/ygor/helpdesk/domain/Pessoa.java
@@ -3,9 +3,13 @@ package com.ygor.helpdesk.domain;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.ygor.helpdesk.domain.enums.Perfil;
 
 import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
@@ -15,42 +19,42 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
-import org.hibernate.validator.constraints.br.CPF;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.ygor.helpdesk.domain.enums.Perfil;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@EqualsAndHashCode(of = { "id", "cpf" })
 public abstract class Pessoa implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	protected Integer id;
+
 	protected String nome;
-	
+
 	@CPF(message = "CPF inválido")
 	@Column(unique = true)
 	protected String cpf;
-	
+
 	@Column(unique = true)
 	protected String email;
+
 	protected String senha;
-	
+
 	@ElementCollection(fetch = FetchType.EAGER)
 	@CollectionTable(name = "PERFIS")
 	protected Set<Integer> perfis = new HashSet<>();
-	
+
 	@JsonFormat(pattern = "dd/MM/yyyy")
 	protected LocalDate dataCriacao = LocalDate.now();
-	
 
 	public Pessoa() {
 		addPerfil(Perfil.CLIENTE);
-		
 	}
-
 
 	public Pessoa(Integer id, String nome, String cpf, String email, String senha) {
 		this.id = id;
@@ -61,95 +65,12 @@ public abstract class Pessoa implements Serializable {
 		addPerfil(Perfil.CLIENTE);
 	}
 
-
-	public Integer getId() {
-		return id;
-	}
-
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-
-	public String getNome() {
-		return nome;
-	}
-
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-
-	public String getCpf() {
-		return cpf;
-	}
-
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-
-
-	public String getEmail() {
-		return email;
-	}
-
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-
-	public String getSenha() {
-		return senha;
-	}
-
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-
-
 	public Set<Perfil> getPerfis() {
 		return perfis.stream().map(Perfil::toEnum).collect(Collectors.toSet());
 	}
 
-
 	public void addPerfil(Perfil perfil) {
 		this.perfis.add(perfil.getCodigo());
 	}
-
-
-	public LocalDate getDataCriacao() {
-		return dataCriacao;
-	}
-
-
-	public void setDataCriacao(LocalDate dataCriacao) {
-		this.dataCriacao = dataCriacao;
-	}
-
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(cpf, id);
-	}
-
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Pessoa other = (Pessoa) obj;
-		return Objects.equals(cpf, other.cpf) && Objects.equals(id, other.id);
-	}
-	
-	
-
 }
+
