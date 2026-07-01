cb8629e6f3850d8d9a67f1be2b74e1497511c3a1
Ygor César de Carvalho <109998784+ygor-ccarvalho@users.noreply.github.com>
2026-07-01 17:15:23 -0300

    refactor: aplica lombok na entidade Cliente

--- src/main/java/com/ygor/helpdesk/domain/Cliente.java
+++ src/main/java/com/ygor/helpdesk/domain/Cliente.java
@@ -11,46 +11,39 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.ygor.helpdesk.domain.dtos.ClienteDTO;
 import com.ygor.helpdesk.domain.enums.Perfil;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 @Entity
-public class Cliente extends Pessoa{
+public class Cliente extends Pessoa {
 
-	private static final long serialVersionUID = 1L;
-	
-	@JsonIgnore
-	@OneToMany(mappedBy = "cliente")
-	private List<Chamado> chamados = new ArrayList<>();
+    private static final long serialVersionUID = 1L;
 
-	public Cliente() {
-		super();
-		addPerfil(Perfil.CLIENTE);
-	}
-	
-	public Cliente(ClienteDTO obj) {
-		super();
-		this.id = obj.getId();
-		this.nome = obj.getNome();
-		this.cpf = obj.getCpf();
-		this.email = obj.getEmail();
-		this.senha = obj.getSenha();
-		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
-		this.dataCriacao = obj.getDataCriacao();
-		addPerfil(Perfil.CLIENTE);
-	}
+    @JsonIgnore
+    @OneToMany(mappedBy = "cliente")
+    private List<Chamado> chamados = new ArrayList<>();
 
-	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
-		super(id, nome, cpf, email, senha);
-		
-		addPerfil(Perfil.CLIENTE);
-	}
+    public Cliente() {
+        super();
+        addPerfil(Perfil.CLIENTE);
+    }
 
-	public List<Chamado> getChamados() {
-		return chamados;
-	}
+    public Cliente(Integer id, String nome, String cpf, String email, String senha) {
+        super(id, nome, cpf, email, senha);
+        addPerfil(Perfil.CLIENTE);
+    }
 
-	public void setChamados(List<Chamado> chamados) {
-		this.chamados = chamados;
-	}
-	
-	
-	
+    public Cliente(ClienteDTO obj) {
+        super();
+        this.id = obj.getId();
+        this.nome = obj.getNome();
+        this.cpf = obj.getCpf();
+        this.email = obj.getEmail();
+        this.senha = obj.getSenha();
+        this.perfis = obj.getPerfis().stream().map(Perfil::getCodigo).collect(Collectors.toSet());
+        this.dataCriacao = obj.getDataCriacao();
+        addPerfil(Perfil.CLIENTE);
+    }
 }
